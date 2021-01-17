2298541b3561c54924953647345f820af6f5c575
helen <helensun@mit.edu>
2021-01-17 01:09:06 -0500

    initial navbar code and login components?

--- client/src/components/modules/NavBar.js
+++ client/src/components/modules/NavBar.js
@@ -1,1 +1,41 @@
 //The side navbar with links to profile, and probably others 
+
+import React, {Component} from "react";
+
+//importing stylistic components
+import "./NavBar.css";
+
+//navigation bar takes no props, always at side of page when on board page
+class NavBar extends Component {
+    constructor(props) {
+        super(props);
+    }
+}
+
+//each title should correspond to different link and different location
+render() {
+    return(
+        <nav className="NavBar-container">
+            <div className="NavBar-title">activiTag</div>
+            <div className="NavBar-title">profile</div>
+            <div className="NavBar-linkContainer">
+                <Link to="/" className="NavBar-link">
+                    profile
+                </Link>
+                {this.props.userId && (
+                    <Link to={`/profile/${this.props.userId}`} className="NavBar-link">
+                        profile
+                    </Link>
+                    //not sure if this is how to link to a different page? should ask about this*
+                )}
+            </div>
+            <div className="NavBar-title">search</div>
+            <div className="NavBar-linkContainer">
+                <Link to="/" className="NavBar-link">
+                    search
+                </Link>
+            </div>
+        </nav>
+    );
+}
+//error with ; but i have no idea where its supposed to go ??
--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,3 +1,23 @@
 //first login screen
 
+//allows user to enter username and record username
+class username extends React.Component {
+    render () {
+        return (
+            <loginTitle title={"Login"}>
+                <div className="container">
+                    <div className="row">
+                        <div className="m-16">
+                            <h2>login</h2>
+                            <hr />
+                        </div>
+                        <LoginForm />
+    
+                    </div>
+                </div>
+            </loginTitle>
 
+        );
+        
+    }
+}
